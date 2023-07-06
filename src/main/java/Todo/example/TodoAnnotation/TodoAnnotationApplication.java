aac47ee1d8744de51235e452c24d340f7c8a5a97
Mantu <mantukumar803204@gmail.com>
2023-07-06 09:19:19 +0530

    fist commit

---
+++ src/main/java/Todo/example/TodoAnnotation/TodoAnnotationApplication.java
@@ -0,0 +1,13 @@
+package Todo.example.TodoAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Todo/example/TodoAnnotation/TodoAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package Todo.example.TodoAnnotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
